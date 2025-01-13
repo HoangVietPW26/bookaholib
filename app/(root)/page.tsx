c391a0912ed08f3f230ebe4f85e05aea5b5a0e61
HoangVietPW26 <nhviet26@gmail.com>
2025-01-13 10:47:17 +0000

    home UI

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,11 +2,17 @@ import Image from "next/image";
 import { Button } from "@/components/ui/button";
 import BookOverview from "@/components/BookOverview";
 import BookList from "@/components/BookList";
+import { sampleBooks } from "../constants";
+
 const Home = () => {
   return (
     <>
-    <BookOverview />
-    <BookList/>
+    <BookOverview {...sampleBooks[0]} />
+    <BookList 
+    title="Latest Book"
+    books={sampleBooks}
+    containerClassName="mt-28"
+    />
     </>
   );
 }
