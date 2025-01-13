4e7ec06881ce8aa294626eb3db0b56f4488a0ee3
HoangVietPW26 <nhviet26@gmail.com>
2025-01-13 07:00:17 +0000

    home layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
-
+import BookOverview from "@/components/BookOverview";
+import BookList from "@/components/BookList";
 const Home = () => {
   return (
     <>
-    <Button>Click me</Button>
+    <BookOverview />
+    <BookList/>
     </>
   );
 }
