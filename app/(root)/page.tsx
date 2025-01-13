12944d0c5cabb71f29332e7b7a8b22036e86575f
HoangVietPW26 <nhviet26@gmail.com>
2025-01-13 22:11:15 +0000

    setup postgres on neon and drizzle ORM

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,8 +3,12 @@ import { Button } from "@/components/ui/button";
 import BookOverview from "@/components/BookOverview";
 import BookList from "@/components/BookList";
 import { sampleBooks } from "../constants";
+import { db } from "@/database/drizzle";
+import {users} from "@/database/schema"
 
-const Home = () => {
+const Home = async() => {
+  const res = await db.select().from(users)
+  console.log(JSON.stringify(res, null, 2))
   return (
     <>
     <BookOverview {...sampleBooks[0]} />
