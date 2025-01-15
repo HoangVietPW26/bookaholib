f41cd9750d02d4bcf0992705f846c6a846af0e83
HoangVietPW26 <nhviet26@gmail.com>
2025-01-15 18:13:38 +0000

    fix deploy

--- components/BookOverview.tsx
+++ components/BookOverview.tsx
@@ -31,8 +31,6 @@ const BookOverview = async({
     .where(eq(users.id, userId))
     .limit(1);
 
-    if (!user) return null
-
     const borrowingEligibility = {
       isEligible: availableCopies > 0 && user?.status === "APPROVED",
       message:
@@ -65,11 +63,11 @@ const BookOverview = async({
         </div>
         <p className='book-description'>{description}</p>
 
-        <BorrowBook 
+        {user && <BorrowBook 
           bookId={id}
           userId={userId}
           borrowingEligibility={borrowingEligibility}
-        />
+        />}
       </div>
 
       <div className="relative flex flex-1 justify-center">
