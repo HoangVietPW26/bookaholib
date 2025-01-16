a5c9d176d9874f9d5d765b702bbe9f71bd64e8ac
HoangVietPW26 <nhviet26@gmail.com>
2025-01-16 02:40:17 +0000

    fx sign out

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
-import { signOut } from 'next-auth/react'
+import { signOut } from '@/auth'
 import { Button } from './ui/button'
 
 const Header = () => {
